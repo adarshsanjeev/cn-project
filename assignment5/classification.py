567273fe833e0838772a044d9faeffcd8a94287f
Adarsh Sanjeev <adarshsanjeev@gmail.com>
2017-11-10 22:09:42 +0000

    Added cost

--- assignment5/classification.py
+++ assignment5/classification.py
@@ -7,15 +7,22 @@ class Node:
 
     def __str__(self):
         return "(Left:%s  Right:%s Next:%s)" % (str(self.left), str(self.right), str(self.next_trie))
+    def __repr__(self):
+        return self.__str__()
 
 root = Node()
 
 class Rule:
-    def __init__(self, number):
+    def __init__(self, number, cost):
         self.number = number
+        self.cost = cost
 
     def __str__(self):
-        return "Rule number:%d" % self.number
+        return "Rule number:%d Cost:%d" % (self.number, self.cost)
+    def __repr__(self):
+        return self.__str__()
+    def __lt__(self, other):
+        return self.cost < other.cost
 
 def dfs(A,B):
     if A.left == None and B.left:
@@ -27,7 +34,6 @@ def dfs(A,B):
     if A.right and B.right:
         dfs(A.right, B.right)
 
-
 def mergeTries(A, B):
     if A == None and B != None:
         return B
@@ -38,7 +44,6 @@ def mergeTries(A, B):
     else:
         dfs(A,B)
         return A
-
 
 def addRule(rule, t1, t2):
     node = root
@@ -68,7 +73,7 @@ def addRule(rule, t1, t2):
                 node.right = Node()
             node = node.right
     if node.next_trie is None:
-        node.next_trie = Rule(rule)
+        node.next_trie = rule
 
 def classify(t1, t2):
     possible_rules = []
@@ -103,14 +108,15 @@ def classify(t1, t2):
 
 
 for index, line in enumerate(open("rules.txt")):
-    t1, l1, t2, l2 = line.split(" ")
+    rule, t1, l1, t2, l2 = line.split(" ")
+    rule = int(rule)
     l1 = int(l1)
     l2 = int(l2)
     bin_t1 =  "".join([ "%08d" % int(str(bin(int(i)))[2:]) for i in t1.split(".") ])
     bin_t2 =  "".join([ "%08d" % int(str(bin(int(i)))[2:]) for i in t2.split(".") ])
     bin_t1 = bin_t1[:l1]
     bin_t2 = bin_t2[:l2]
-    addRule(index+1, bin_t1, bin_t2)
+    addRule(Rule(rule, index), bin_t1, bin_t2)
 
 with open("output.txt", 'w') as O:
     for index, line in enumerate(open("input")):
@@ -118,4 +124,7 @@ with open("output.txt", 'w') as O:
         bin_t1 =  "".join([ "%08d" % int(str(bin(int(i)))[2:]) for i in t1.split(".") ])
         bin_t2 =  "".join([ "%08d" % int(str(bin(int(i)))[2:]) for i in t2.split(".") ])
         possible_rules = classify(bin_t1, bin_t2)
-        O.write('%s %s %d %d\n' % (t1, t2, len(possible_rules), min(possible_rules+[Rule(-1)]).number))
+        if possible_rules == []:
+            O.write('%s %s %d %d\n' % (t1, t2, len(possible_rules), -1))
+        else:
+            O.write('%s %s %d %d\n' % (t1, t2, len(possible_rules), min(possible_rules).number))
